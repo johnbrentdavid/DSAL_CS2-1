1cfcfc9c64500e08e298b1430673c6700c6db254
Eusugii <johnbrent725@gmail.com>
2022-01-28 16:47:12 +0800

    new init

---
+++ OE2_David.java
@@ -0,0 +1,11 @@
+public class OE2_David {
+    
+    public static void bubbleSort(int[] num1){
+        int ctr = num1.length;
+    }
+
+    public static public static void main(String[] args) {
+        
+
+    }
+}
